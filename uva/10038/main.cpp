7630067bd1e6dfd293354b641687ee23cab0a564
Jhon Cardenas <ext-jhon.cardenas@here.com>
2019-05-20 23:20:42 -0500

    UVA 10038: Jully Jumpers solution

---
+++ uva/10038/main.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+#include <set>
+#include <math.h>
+
+/**
+ * UVA 10038: Jully Jumpers
+ *
+ * Time complexity:
+ *   O(n log n)
+ *
+ * Space complexity:
+ *   O(n)
+ * */
+
+using namespace std;
+
+bool is_sequence_a_jolly_jumper(int *sequence, unsigned int n)
+{
+    if (n == 1)
+        return true;
+    
+    set<int> differences;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int difference = abs(sequence[i] - sequence[i + 1]);
+        if (1 <= difference && difference <= n - 1) {
+            // complexity generally: log n
+            differences.insert(difference);
+        }
+        if (differences.size() != (i +  1)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    unsigned int n;
+    while (cin >> n && n > 0)
+    {
+        int sequence[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> sequence[i];
+        }
+        cout << (is_sequence_a_jolly_jumper(sequence, n) ? "Jolly" : "Not jolly") << endl;
+    }
+    return 0;
+}
