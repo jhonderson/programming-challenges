08408b5ba1d3aeb8017e30628387b8e3981d1a5b
Jhon Cardenas <ext-jhon.cardenas@here.com>
2019-05-27 13:25:53 -0500

    Solution to UVA 10911 Forming Quiz Teams, WIP, current answer is TLE

---
+++ uva/10911/main.cpp
@@ -0,0 +1,85 @@
+
+#include <iostream>
+#include <cmath>
+#include <string>
+
+#define MAX_ANSWER 1500 * 16
+
+/**
+ * UVA 10911: Forming quiz teams
+ *
+ * Time complexity:
+ *
+ * Space complexity:
+ * */
+
+using namespace std;
+
+double find_distance(int* x, int* y, unsigned int i, unsigned int j)
+{
+    return sqrt((x[j]-x[i]) * (x[j]-x[i]) + (y[j]-y[i]) * (y[j]-y[i]));
+}
+
+double find_minimum_distance_sum_recursivelly(int* x, int* y, unsigned int n, bool* visited_points)
+{
+    double answer = MAX_ANSWER;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n && !visited_points[i]; j++)
+        {
+            if (visited_points[j])
+                continue;
+            visited_points[i] = true;
+            visited_points[j] = true;
+
+            double points_distance = find_distance(x, y, i, j);
+            if (points_distance < answer) {
+                double current_answer = points_distance + find_minimum_distance_sum_recursivelly(x, y, n, visited_points);
+                answer = min(answer, current_answer);
+            }
+
+            visited_points[i] = false;
+            visited_points[j] = false;
+        }
+    }
+    return answer == MAX_ANSWER ? 0 : answer;
+}
+
+double find_minimum_distance_sum(int* x, int* y, unsigned int n)
+{
+    bool visited_points[n];
+    for (int i = 0; i < n; i++)
+        visited_points[i] = false;
+    return find_minimum_distance_sum_recursivelly(x, y, n, visited_points);
+}
+
+int main()
+{
+    unsigned int caseNumber = 0;
+    unsigned int n;
+
+    string name;
+
+    while(cin >> n)
+    {
+        if (n <= 0)
+            break;
+
+        caseNumber++;
+        n *= 2;
+
+        unsigned i = 0;
+        int x[n], y[n];
+
+        while(i < n)
+        {
+            cin >> name >> x[i] >> y[i];
+            i++;
+        }
+
+        double answer = find_minimum_distance_sum(x, y, n);
+
+        printf("Case %d: %.2lf\n", caseNumber, answer);
+    }
+    return 0;
+}
