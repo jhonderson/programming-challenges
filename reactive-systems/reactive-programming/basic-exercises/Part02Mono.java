d99efadf35b0b2aefc7d4479c8d1686f22100d80
Jhon Anderson Cardenas Diaz <jhon@pop-os.localdomain>
2021-03-24 17:44:27 -0700

    Basic reactive programming exercises

---
+++ reactive-systems/reactive-programming/basic-exercises/Part02Mono.java
@@ -0,0 +1,48 @@
+package io.pivotal.literx;
+
+//generic imports to help with simpler IDEs (ie tech.io)
+import java.util.*;
+import java.util.function.*;
+import java.time.*;
+
+import reactor.core.publisher.Mono;
+
+// https://tech.io/playgrounds/929/reactive-programming-with-reactor-3/Mono
+
+/**
+ * Learn how to create Mono instances.
+ *
+ * @author Sebastien Deleuze
+ * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html">Mono Javadoc</a>
+ */
+public class Part02Mono {
+
+//========================================================================================
+
+	// TODO Return an empty Mono
+	Mono<String> emptyMono() {
+		return Mono.empty();
+	}
+
+//========================================================================================
+
+	// TODO Return a Mono that never emits any signal
+	Mono<String> monoWithNoSignal() {
+		return Mono.never();
+	}
+
+//========================================================================================
+
+	// TODO Return a Mono that contains a "foo" value
+	Mono<String> fooMono() {
+		return Mono.just("foo");
+	}
+
+//========================================================================================
+
+	// TODO Create a Mono that emits an IllegalStateException
+	Mono<String> errorMono() {
+		return Mono.error(new IllegalStateException());
+	}
+
+}
